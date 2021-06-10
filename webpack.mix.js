7c923176849ab9a9c0054a5b9766f9863f1c0f1a
Rizal muhamad H <rizalmuhamadh@gmail.com>
2021-06-10 21:32:21 +0700

    plugins

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,4 @@
-const mix = require('laravel-mix');
+const mix = require("laravel-mix");
 
 /*
  |--------------------------------------------------------------------------
@@ -11,7 +11,20 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+// mix.js('resources/js/app.js', 'public/js')
+//     .postCss('resources/css/app.css', 'public/css', [
+//         //
+//     ]);
+mix.styles(
+    ["resources/css/style.css", "resources/css/components.css"],
+    "public/assets/css/all.css"
+).version();
+
+mix.js("resources/js/scripts.js", "public/assets/js/scripts.js")
+    .js("resources/js/custom.js", "public/assets/js/custom.js")
+    .js("resources/js/stisla.js", "public/assets/js/stisla.js")
+    .version();
+mix.copy(
+    "node_modules/bootstrap-social/bootstrap-social.css",
+    "public/assets/css/bootstrap-social/bootstrap-social.css"
+);
