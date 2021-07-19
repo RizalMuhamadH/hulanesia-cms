eff6a0f0ba2257f60f29de4da237f9f2b01bb63a
Rizal Muhamad H <rizalmuhamadh@gmail.com>
2021-07-19 16:53:20 +0700

    post schedule

---
+++ app/Jobs/PostSchedule.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+use App\Models\Post;
+
+class PostSchedule implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+
+    protected $post;
+
+    public function __construct($post)
+    {
+        $this->post = $post;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        Post::where('id', $this->post->id)->update(['status' => 'PUBLISH']);
+    }
+}
