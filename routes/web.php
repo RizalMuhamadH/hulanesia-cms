c470f380f225a22d91a442cbc3ec14cb0de598b6
Rizal Muhamad H <rizalmuhamadh@gmail.com>
2021-07-29 11:07:02 +0700

    redirect

--- routes/web.php
+++ routes/web.php
@@ -23,9 +23,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+    return redirect('login');
+});
 
 Route::middleware(['auth'])->group(function () {
 
