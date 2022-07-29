d56b71767b92ca6a0e5dce9490aa46602669f49b
Rizal muhamad H <rizalmuhamadh@gmail.com>
2022-07-29 11:05:47 +0700

    update logging action

--- config/logging.php
+++ config/logging.php
@@ -57,8 +57,8 @@ return [
         'slack' => [
             'driver' => 'slack',
             'url' => env('LOG_SLACK_WEBHOOK_URL'),
-            'username' => 'Laravel Log',
-            'emoji' => ':boom:',
+            'username' => env('LOG_TITLE'),
+            'emoji' => ':loudspeaker:',
             'level' => env('LOG_LEVEL', 'critical'),
         ],
 
